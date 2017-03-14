41ce2e4f18732b679a863d58d0c5ba0b828d592e
da0818 <chuntawu@gmail.com>
2017-03-14 20:08:55 +0800

    Example01初使狀態

---
+++ Example01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Status response = Status.error;
+
+            switch (response)
+            {
+                case Status.error:
+                    //你要寫的程式
+                    Console.WriteLine("狀態成功");
+                    Console.WriteLine((int)response);
+                    break;
+
+                case Status.success:
+                    //你要寫的程式
+                    Console.WriteLine("狀態錯誤");
+                    Console.WriteLine((int)response);
+                    break;
+
+                default:
+                    //你要寫的程式
+                    Console.WriteLine("狀態不明");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
